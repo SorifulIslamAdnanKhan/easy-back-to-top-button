438fe396b84a58c26e3f1728538442e7f4b80c45
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2024-12-16 10:27:12 +0600

    initial plugin setup

---
+++ easy-back-to-top-button.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*
+ * Plugin Name:       Easy Back To Top Button
+ * Plugin URI:        https://wordpress.org/plugins/easy-back-to-top-button/
+ * Description:       It's a handy tool to add an easy back to top button in your website.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Adnan Khan
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/SorifulIslamAdnanKhan
+ * Text Domain:       ebttb
+ */
+?>
