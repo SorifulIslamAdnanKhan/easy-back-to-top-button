67699bc50988c69c80776d588522810e57b10776
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2025-01-05 12:16:28 +0600

    author name updated

--- easy-back-to-top-button.php
+++ easy-back-to-top-button.php
@@ -7,7 +7,7 @@
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Sornix
+ * Author:            Sorinx
  * Author URI:        https://sorinx.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
